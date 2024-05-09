6489f12b315bb0fa14641fdc78b0c6435ab6e950
Kevin Corasaniti <kevincorasaniti@gmail.com>
2024-05-09 14:54:20 +0200

    init

---
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/JokiBackendApplication.java
@@ -0,0 +1,13 @@
+package unical.enterprise.jokibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokiBackendApplication.class, args);
+    }
+
+}
---
+++ JokiBackend/src/test/java/unical/enterprise/jokibackend/JokiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unical.enterprise.jokibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
