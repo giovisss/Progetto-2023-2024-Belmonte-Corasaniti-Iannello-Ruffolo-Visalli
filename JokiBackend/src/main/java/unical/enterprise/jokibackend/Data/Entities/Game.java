15f52763c52e8c7c0a985c4dd71a5e33c33af4f8
YanMark42 <iannellomarco@gmail.com>
2024-05-15 17:41:43 +0200

    create entità User, Game e Achievement

---
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/Game.java
@@ -0,0 +1,54 @@
+package unical.enterprise.jokibackend.Data.Entities;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Data // lombok, genera getter e setter
+@Entity(name="games") // jakarta, fa riferimento alla tabella games
+public class Game {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    @Column(name = "game_id")
+    private Long game_id;
+
+    @Column(name = "game_name")
+    private String game_name;
+
+    @Column(name = "game_description")
+    private String game_description;
+
+    @Column(name = "game_price")
+    private double game_price;
+
+    @Column(name = "game_image")
+    private String game_image; // se vogliamo salvare l'immagine in base64
+
+    @Column(name = "game_category")
+    private String game_category;
+
+    @Column(name = "game_developer")
+    private String game_developer;
+
+    @Column(name = "game_publisher")
+    private String game_publisher;
+
+    @Column(name = "game_release_date")
+    private String game_releaseDate;
+
+    @ManyToMany(mappedBy = "user_games", fetch = FetchType.LAZY) // un gioco può essere giocato da più utenti
+    private List<User> user_games = new ArrayList<>();
+
+    @OneToMany(mappedBy = "game", fetch = FetchType.LAZY) // un gioco può avere più achievement
+    private Set<Achievement> achievements = new HashSet<>();
+}
---
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/User.java
@@ -0,0 +1,43 @@
+package unical.enterprise.jokibackend.Data.Entities;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+
+@Data // lombok, genera getter e setter
+@Entity(name="users") // jakarta, fa riferimento alla tabella users
+public class User {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long user_id;
+
+    @Column(name = "user_password")
+    private String user_password;
+
+    @Column(name = "user_role")
+    private String user_role;
+
+    @Column(name = "user_username", unique = true)
+    private String user_username;
+
+    @ManyToMany(mappedBy = "user_games", fetch = FetchType.LAZY)
+    private List <Game> user_games = new ArrayList<>();
+
+    @ManyToMany(fetch = FetchType.LAZY) // un utente può sbloccare più achievement
+    @JoinTable(name = "user_achievements",
+               joinColumns = @JoinColumn(name = "user_id"),
+               inverseJoinColumns = @JoinColumn(name = "achievement_id"))
+    private Set<Achievement> achievements = new HashSet<>();
+}
