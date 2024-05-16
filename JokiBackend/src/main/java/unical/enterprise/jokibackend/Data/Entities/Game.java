555a885e4e3a5a1bcad38b46f499d9cb92df734e
YanMark42 <iannellomarco@gmail.com>
2024-05-16 16:05:51 +0200

    acsc

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/Game.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/Game.java
@@ -1,19 +1,9 @@
 package unical.enterprise.jokibackend.Data.Entities;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data // lombok, genera getter e setter
--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/User.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Data/Entities/User.java
@@ -1,10 +1,7 @@
 package unical.enterprise.jokibackend.Data.Entities;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
