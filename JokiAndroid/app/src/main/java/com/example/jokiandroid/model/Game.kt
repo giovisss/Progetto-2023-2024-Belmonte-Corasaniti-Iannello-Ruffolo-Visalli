bba311535b3f5e691fd6eb22275722a192c3b7ba
giovisss <giovanni.visalli@libero.it>
2024-06-14 17:24:20 +0200

    aggiornato

--- JokiAndroid/app/src/main/java/com/example/jokiandroid/model/Game.kt
+++ JokiAndroid/app/src/main/java/com/example/jokiandroid/model/Game.kt
@@ -9,18 +9,18 @@ data class Game(
 
 fun getFakeGames(): List<Game>{
     return listOf<Game>(
-        Game(1,"Valorant", 35.0, "Fatti una doccia"),
+        Game(1,"Valorant", 35.0, "Gioco bellissimo"),
         Game(2,"Rainbow six", 35.0, "Best gioco ever"),
-        Game(3,"XDefiant", 35.0, "Gioco di merda"),
-        Game(4,"XDefiant", 35.0, "Gioco di merda"),
-        Game(5,"XDefiant", 35.0, "Gioco di merda"),
-        Game(6,"XDefiant", 35.0, "Gioco di merda"),
-        Game(7,"XDefiant", 35.0, "Gioco di merda"),
-        Game(8,"XDefiant", 35.0,"Gioco di merda"),
-        Game(9,"XDefiant", 35.0,"Gioco di merda"),
-        Game(10,"XDefiant", 35.0,"Gioco di merda"),
-        Game(11,"XDefiant", 35.0,"Gioco di merda"),
-        Game(12,"XDefiant", 35.0,"Gioco di merda"),
-        Game(13,"XDefiant", 35.0,"Gioco di merda")
+        Game(3,"XDefiant", 35.0, "Gioco"),
+        Game(4,"XDefiant", 35.0, "Gioco"),
+        Game(5,"XDefiant", 35.0, "Gioco"),
+        Game(6,"XDefiant", 35.0, "Gioco"),
+        Game(7,"XDefiant", 35.0, "Gioco"),
+        Game(8,"XDefiant", 35.0,"Gioco"),
+        Game(9,"XDefiant", 35.0,"Gioco"),
+        Game(10,"XDefiant", 35.0,"Gioco"),
+        Game(11,"XDefiant", 35.0,"Gioco"),
+        Game(12,"XDefiant", 35.0,"Gioco"),
+        Game(13,"XDefiant", 35.0,"Gioco")
     )
 }
