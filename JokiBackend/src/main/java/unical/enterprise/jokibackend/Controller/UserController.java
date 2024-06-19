1808fea929ab7787dcba38c7f85ad8fa6029ce27
DanteScot <kevincorasaniti@gmail.com>
2024-06-19 18:33:37 +0200

    Update UserController.java

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Controller/UserController.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Controller/UserController.java
@@ -1,16 +1,18 @@
 //package unical.enterprise.jokibackend.Controller;
 //
+//import lombok.RequiredArgsConstructor;
 //import org.springframework.security.access.prepost.PreAuthorize;
 //import org.springframework.security.core.context.SecurityContextHolder;
 //import org.springframework.web.bind.annotation.*;
+//import unical.enterprise.jokibackend.Data.Services.KeycloakService;
 //
 //@RestController
 //@RequestMapping("/api/users")
 //@CrossOrigin(origins = "*")
+//@RequiredArgsConstructor
 //public class UserController {
 //
-//    @PostMapping("/register")
-//    public String register(@RequestBody String username, @RequestBody String password) {
-//        return "register";
-//    }
+//    private final KeycloakService service;
+//
+//
 //}
