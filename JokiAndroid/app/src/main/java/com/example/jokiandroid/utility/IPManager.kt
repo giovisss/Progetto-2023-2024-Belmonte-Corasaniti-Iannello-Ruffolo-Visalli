b8baae2de42e23d5f97915bc6dec25ab1ccd2818
DanteScot <kevincorasaniti@gmail.com>
2024-08-09 17:43:08 +0200

    ip manager

---
+++ JokiAndroid/app/src/main/java/com/example/jokiandroid/utility/IPManager.kt
@@ -0,0 +1,30 @@
+package com.example.jokiandroid.utility
+
+import android.content.Context
+import com.example.jokiandroid.R
+
+object IPManager {
+    var KEYCLOAK_IP: String = ""
+        get() = field
+        set(value) {
+            if (field == "") {
+                field = value
+            }else{
+                throw Exception("Non puoi cambiare il valore di questa variabile")
+            }
+        }
+    var BACKEND_IP: String = ""
+        get() = field
+        set(value) {
+            if (field == "") {
+                field = value
+            }else{
+                throw Exception("Non puoi cambiare il valore di questa variabile")
+            }
+        }
+
+    fun setIps(context: Context){
+        KEYCLOAK_IP = context.getString(R.string.keycloak)
+        BACKEND_IP = context.getString(R.string.backend)
+    }
+}
