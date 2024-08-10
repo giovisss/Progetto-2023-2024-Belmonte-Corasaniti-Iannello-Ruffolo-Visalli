5adee76f314307cd5c854fa1b718f4e2bdc0bfe8
DanteScot <kevincorasaniti@gmail.com>
2024-08-10 17:48:41 +0200

    sistemato login

--- JokiAndroid/app/src/main/java/com/example/jokiandroid/activity/LoginActivity.kt
+++ JokiAndroid/app/src/main/java/com/example/jokiandroid/activity/LoginActivity.kt
@@ -1,11 +1,6 @@
 package com.example.jokiandroid.activity
 
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,48 +8,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.jokiandroid.R
-
-
-class LoginActivity(navController: NavController) : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-        // Handle authentication result
-        handleAuthenticationResult()
-    }
-
-    private fun handleAuthenticationResult() {
-        val intent = intent
-        val data = intent.data
-
-        Log.e("res","data: $data")
-
-        if (data != null && data.scheme == "joki" && data.host == "callback") {
-            // Handle successful authentication
-//            setContent() {
-//                Surface {
-//                    Text(
-//                        modifier = Modifier.padding(top = 50.dp),
-//                        text = "Login"
-//                    )
-//                }
-//            }
-        } else {
-            // Handle error or cancellation
-            Toast.makeText(this, "Authentication failed or cancelled", Toast.LENGTH_SHORT).show()
-        }
-    }
-}
-
 
 
 @Composable
-fun Login(navController: NavController) {
+fun LoginActivity(navController: NavController) {
     Surface {
-        Text( modifier = Modifier.padding(top = 50.dp),
+        Text(
+            modifier = Modifier
+                .padding(top = 50.dp)
+                .fillMaxWidth()
+                .padding(16.dp),
+
             text = "Login"
         )
     }
