30cdab3632d6f67f680122615b69ad571035718d
DanteScot <kevincorasaniti@gmail.com>
2024-08-13 01:41:21 +0200

    fix

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Controller/v1/WishlistController.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Controller/v1/WishlistController.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import unical.enterprise.jokibackend.Controller.UserController;
 import unical.enterprise.jokibackend.Data.Dto.GameDto;
 import unical.enterprise.jokibackend.Data.Services.Interfaces.WishlistService;
 
@@ -19,7 +18,7 @@ public class WishlistController {
 
     private final WishlistService wishlistService;
 
-    Logger logger = Logger.getLogger(UserController.class.getName());
+    Logger logger = Logger.getLogger(WishlistController.class.getName());
 
     @GetMapping(value = "/{username}", produces = "application/json")
     public ResponseEntity<String> getWishlist(@PathVariable String username){
