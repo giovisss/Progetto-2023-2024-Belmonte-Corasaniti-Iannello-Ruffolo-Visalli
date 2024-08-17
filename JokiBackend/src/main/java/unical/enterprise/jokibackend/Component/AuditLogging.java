accd1dbbd0c7b5b5b00923cff66f8dac57628c88
Noemi Ruffolo <ruffolo.noemi@gmail.com>
2024-08-17 16:56:47 +0200

    Revert "Merge branch 'main' of https://github.com/giovisss/progetto-ea"
    This reverts commit 4043ec4c16f3cd55d1d0e8089922154f01f54e49, reversing
    changes made to 81daa11780445ed5dc1494c07c40db0e246f1c37.

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/AuditLogging.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/AuditLogging.java
@@ -4,62 +4,66 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import unical.enterprise.jokibackend.Utility.CustomContextManager.UserContextHolder;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
 
 @Aspect
 @Component
-@Scope(WebApplicationContext.SCOPE_REQUEST)
 @Transactional
 public class AuditLogging {
 
     private static final Logger logger = Logger.getLogger(AuditLogging.class.getName());
-    private static final ThreadLocal<StringBuilder> logBuilder = ThreadLocal.withInitial(StringBuilder::new);
+    private final ReentrantLock lock = new ReentrantLock();
 
     @Pointcut("execution(* unical.enterprise.jokibackend.Controller..*.*(..))")
     public void allControllerMethods() {}
 
     @Before("allControllerMethods()")
     public void logBefore(JoinPoint joinPoint) {
-        StringBuilder builder = logBuilder.get();
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        lock.lock();
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 
-        builder.append("\n----------------------------------------------------------------------------------------------------\n");
+            logger.info("--------------------------------------------------");
 
-        if(request.getUserPrincipal() == null) builder.append("User: Anonymous\n");
-        else builder.append("User: {").append(UserContextHolder.getContext().toShortString()).append("}\n");
+            if(request.getUserPrincipal() == null) logger.info("User: Anonymous");
+            else logger.info("User: {" + UserContextHolder.getContext().toShortString() + "}");
 
-        builder.append("Request Method: ").append(request.getMethod()).append("\n");
-        builder.append("Endpoint: ").append(request.getRequestURI()).append("\n");
-        builder.append("Arguments: ").append(Arrays.toString(joinPoint.getArgs())).append("\n");
-        builder.append("Signature: ").append(joinPoint.getSignature()).append("\n");
+            logger.info("Request Method: " + request.getMethod());
+            logger.info("Endpoint: " + request.getRequestURI());
+
+            logger.info("Arguments: " + Arrays.toString(joinPoint.getArgs()));
+            logger.info("Signature: " + joinPoint.getSignature());
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        }
     }
 
     @AfterReturning(pointcut = "allControllerMethods()", returning = "result")
     public void logAfter(Object result) {
-        StringBuilder builder = logBuilder.get();
-        builder.append("Response: ").append(result).append("\n");
-        builder.append("----------------------------------------------------------------------------------------------------\n");
-        logger.info(builder.toString());
-        logBuilder.remove();
+        try {
+            logger.info("Response: " + result);
+            logger.info("--------------------------------------------------");
+        } catch (Exception e){
+            logger.severe(e.getMessage());
+        } finally {
+            lock.unlock();
+        }
     }
 
     @AfterThrowing(pointcut = "allControllerMethods()", throwing = "error")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable error) {
-        StringBuilder builder = logBuilder.get();
-        builder.append("****************************************************************************************************\n");
-        builder.append("AN EXCEPTION HAS BEEN THROWN\n");
-        builder.append("Cause : ").append(error).append("\n");
-        builder.append("****************************************************************************************************\n");
-        builder.append("----------------------------------------------------------------------------------------------------");
-        logger.info(builder.toString());
-        logBuilder.remove();
+        logger.info("**************************************************");
+        logger.severe("AN EXCEPTION HAS BEEN THROWN");
+        logger.severe("Cause : " + error);
+        logger.info("**************************************************");
+        logger.info("--------------------------------------------------");
+        lock.unlock();
     }
 }
--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Utility/CustomContextManager/UserContextFilter.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Utility/CustomContextManager/UserContextFilter.java
@@ -2,8 +2,13 @@
 
  import com.nimbusds.jwt.JWTClaimsSet;
  import com.nimbusds.jwt.SignedJWT;
- import jakarta.servlet.*;
+ import jakarta.servlet.Filter;
+ import jakarta.servlet.FilterChain;
+ import jakarta.servlet.ServletException;
+ import jakarta.servlet.ServletRequest;
+ import jakarta.servlet.ServletResponse;
  import jakarta.servlet.http.HttpServletRequest;
+ import java.io.IOException;
 
  public class UserContextFilter implements Filter {
 
@@ -11,7 +16,7 @@
  //    public void init(FilterConfig filterConfig) throws ServletException {}
 
      @Override
-     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
          try {
  //            TODO: GESTIRE BENE ECCEZIONE
 
