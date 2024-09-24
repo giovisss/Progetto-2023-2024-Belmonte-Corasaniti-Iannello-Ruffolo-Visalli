43c8c46d5eca8d1d3b71f8e5d385ea12bc87adba
DanteScot <kevincorasaniti@gmail.com>
2024-09-24 17:35:45 +0200

    fix

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/UserPermissionEvaluator.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/UserPermissionEvaluator.java
@@ -1,29 +1,29 @@
-package unical.enterprise.jokibackend.Component;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Component;
-import unical.enterprise.jokibackend.Data.Services.Interfaces.UserService;
-
-@Component
-@RequiredArgsConstructor
-public class UserPermissionEvaluator {
-    private final UserService userService;
-
-    public boolean isFriend(Authentication auth, Object targetUser) {
-        // Implementa il controllo dell'amicizia tra utenti
-        String user=auth.getName();
-        String target=targetUser.toString();
-
-        if(user.equals(target)) return true;
-
-        return (userService.getFriendByUsername(user, target) != null) &&
-                (userService.getFriendByUsername(target,user) != null);
-    }
-
-    public boolean canVisualize(Object targetUser) {
-        // Implementa il controllo della visibilità del profilo
-
-        return true;
-    }
-}
+//package unical.enterprise.jokibackend.Component;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Component;
+//import unical.enterprise.jokibackend.Data.Services.Interfaces.UserService;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class UserPermissionEvaluator {
+//    private final UserService userService;
+//
+//    public boolean isFriend(Authentication auth, Object targetUser) {
+//        // Implementa il controllo dell'amicizia tra utenti
+//        String user=auth.getName();
+//        String target=targetUser.toString();
+//
+//        if(user.equals(target)) return true;
+//
+//        return (userService.getFriendByUsername(user, target) != null) &&
+//                (userService.getFriendByUsername(target,user) != null);
+//    }
+//
+//    public boolean canVisualize(Object targetUser) {
+//        // Implementa il controllo della visibilità del profilo
+//
+//        return true;
+//    }
+//}
