287873a9c4a6f244bfacb6e41400cdbcb04fed1d
Noemi Ruffolo <ruffolo.noemi@gmail.com>
2024-10-10 13:44:51 +0200

    Modifica alla UI della pagina

--- JokiFrontend/src/app/component/user-info/user-info.component.ts
+++ JokiFrontend/src/app/component/user-info/user-info.component.ts
@@ -15,27 +15,9 @@ export class UserInfoComponent implements OnInit {
   user: User | null = null;
   formattedBirthdate: string | null = null;
   userForm!: FormGroup;
+  showEditForm = false;
 
   constructor(private keycloakService: KeycloakService, private userService: UserService, private fb: FormBuilder) { }
-
-//   ngOnInit(): void {
-//     this.userService.getUserInfo().subscribe(data => {
-//       console.log('Fetched user data:', data); // Debugging line
-//       this.userService.setUser(data); // Imposta l'utente nel servizio
-//       this.user = this.userService.getUser(); // Recupera l'utente
-//       this.formattedBirthdate = this.userService.getFormattedBirthdate(); // Ottiene la data formattata
-//       }, error => {
-//       console.error('Error fetching user data:', error); // Error handling
-//     });
-//     //this.user = this.keycloakService.getKeycloakInstance().tokenParsed;
-//     this.userForm = this.fb.group({
-//       firstName: ['', Validators.maxLength(50)],    // Non obbligatorio
-//       lastName: ['', Validators.maxLength(50)],     // Non obbligatorio
-//       email: ['', [Validators.email]],              // Non obbligatorio ma deve essere un'email valida se presente
-//       password: ['', Validators.minLength(6)],      // Non obbligatorio ma minimo 6 caratteri se presente
-//       repeatpassword: ['', Validators.minLength(6)] // Non obbligatorio ma minimo 6 caratteri se presente
-//     });
-//   }
 
   ngOnInit(): void {
     // Inizializza il form con campi vuoti o placeholder
