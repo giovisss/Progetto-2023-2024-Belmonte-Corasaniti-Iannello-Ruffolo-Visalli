4d258334a7bc393008604f1ee26d77b7194c8cdc
YanMark42 <iannellomarco@gmail.com>
2024-10-30 12:25:46 +0100

    aggiunta dipendenza e exception handler per validazione dati

--- JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/GlobalExceptionHandler.java
+++ JokiBackend/src/main/java/unical/enterprise/jokibackend/Component/GlobalExceptionHandler.java
@@ -1,12 +1,23 @@
 package unical.enterprise.jokibackend.Component;
 
 import com.google.gson.Gson;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import net.minidev.json.JSONObject;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import unical.enterprise.jokibackend.Exceptions.NotModifiedException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.NotFoundException;
 
@@ -41,5 +52,28 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> handleException(Exception ignoredE) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Gson().toJson("An unexpected error occurred"));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    private ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<String> errorMessages = ex.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .toList();
+
+        return new ResponseEntity<>(new JSONObject(
+                Map.of("errors", errorMessages)).toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(new Gson().toJson(errors));
     }
 }
